45d0c3a422de16f1355cd7b4a26b0e13007d2271
TuanPh1608 <ptuan160804@gmail.com>
2025-03-23 23:04:44 +0700

    Fetch data and setup-sanity

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -3,10 +3,12 @@ import { EyeIcon } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { Button } from './ui/button'
+import { Author, Startup } from '@/sanity/types'
 
+export type StartupTypeCard = Omit<Startup, "author"> & {author?: Author}
 
 const StartupCard = ({ post }: { post: StartupTypeCard }) => {
-    const { _createdAt, views, author: { _id: authorId, name }, _id, title, catergory, image, description } = post
+    const { _createdAt, views, author, _id, title, category, image, description } = post
     return (
         <li className='startup-card group'>
             <div className='flex-between'>
@@ -20,14 +22,14 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
             </div>
             <div className='flex-between mt-5 gap-5'>
                 <div className="flex-1">
-                    <Link href={`/user/${authorId}`}>
-                        <p className="text-16-medium line-clamp-1">{name}</p>
+                    <Link href={`/user/${author?._id}`}>
+                        <p className="text-16-medium line-clamp-1">{author?.name}</p>
                     </Link>
                     <Link href={`/startup/${_id}`}>
                         <h3 className="text-26-semibold line-clamp-1">{title}</h3>
                     </Link>
                 </div>
-                <Link href={`/user/${authorId}`}>
+                <Link href={`/user/${author?._id}`}>
                     <Image src="https://placehold.co/48x48" alt="Placeholder" width={48} height={48}
                         className="rounded-full" />
                 </Link>
@@ -35,8 +37,8 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
             <Link href={`/startup/${_id}`}>
                 <p className='startup-card-desc'>{description}</p>
                 <Image
-                    src={image}
-                    alt={title}
+                    src={image || "https://placehold.co/100x75"}
+                    alt={title || "Default title"}
                     className="startup-card_img mt-2"
                     layout="responsive"
                     width={100}
@@ -44,8 +46,8 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
                 />
             </Link>
             <div className="flex-between gap-3 mt-5">
-                <Link href={`/query=${catergory.toLowerCase()}`}>
-                    <p className='text-16-medium'>{catergory}</p>
+                <Link href={`/query=${category?.toLowerCase()}`}>
+                    <p className='text-16-medium'>{category}</p>
                 </Link>
                 <Button className='startup-card_btn' asChild>
                     <Link href={`/startup/${_id}`}>Details</Link>
