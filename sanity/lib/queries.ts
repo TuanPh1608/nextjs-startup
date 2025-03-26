32bd934f00a284d68958b020398ab0494a717f0d
TuanPh1608 <ptuan160804@gmail.com>
2025-03-26 21:51:33 +0700

    Auth flow

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -38,12 +38,12 @@ export const STARTUP_VIEW_QUERY =
 }`);
 
 export const AUTHOR_BY_GITHUB_ID_QUERY =
-  defineQuery(`*[_type == "author" && id == $id] [0] {
-  _id,
-  id,
-  name,
-  username,
-  email,
-  image,
-  bio,
-  }`);
+  defineQuery(`*[_type == "author" && id == $id][0]{
+      _id,
+      id,
+      name,
+      username,
+      email,
+      image,
+      bio
+}`);
